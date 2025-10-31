65491a0c4d43715e460f82444cd39b35e405fbdb
bebop80 <35691815+bebop80@users.noreply.github.com>
2025-10-31 17:08:56 +0100

    Fix end time for guaranteed time band

--- src/App.js
+++ src/App.js
@@ -258,7 +258,7 @@ const strikeRules = {
   strikeDate: '2025-11-14', // Data di sciopero: 14 Novembre 2025
   guaranteedTimeBands: [
     { start: '00:01', end: '12:59' }
-    { start: '17:00', end: '00:59' }
+    { start: '17:00', end: '23:59' }
      ],
   //guaranteedTimeBands: [
     //{ start: '07:00', end: '10:00' },
